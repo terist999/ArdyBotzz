d5cbc9c1556d48d164519a945fda1459093ec4c4
terist999 <xscammer25@gmail.com>
2025-07-19 00:20:42 -0800

    Update NutHub.lua

--- NutHub.lua
+++ NutHub.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/hP50lq7IhA/0", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))()
